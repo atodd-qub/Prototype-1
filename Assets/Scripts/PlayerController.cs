d5a31853e7d23f9413f3bd6f2f173595fbb55a5c
atodd-qub <atodd11@qub.ac.uk>
2023-10-12 16:33:24 +0100

    Finish Lab 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,23 +8,34 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 40.0f;
     private float horizontalInput;
     private float forwardInput;
-    
+    public string inputID;
+    public Camera mainCam;
+    public Camera frontCam;
+    public KeyCode switchKey;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam.enabled = true;
+        frontCam.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //This is where we get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
         
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCam.enabled = !mainCam.enabled;
+            frontCam.enabled = !frontCam.enabled;
+        }
     }
 }
